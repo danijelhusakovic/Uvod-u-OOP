3feaf747eb0f4126fa6c6b89910feefd23d4c819
danijelhusakovic <danijel.husakovic@gmail.com>
2025-10-07 10:40:34 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Prvi_projekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
