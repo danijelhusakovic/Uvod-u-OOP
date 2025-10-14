2072e48dd3c422580796b4cab4f25a0abb007eaa
danijelhusakovic <danijel.husakovic@gmail.com>
2025-10-14 16:54:15 +0200

    Created Pravokutnik.cs

---
+++ Pravokutnik.cs
@@ -0,0 +1,21 @@
+﻿public class Pravokutnik : GeometrijskiLik
+{
+    public Tocka a;
+    public Tocka c;
+
+    public Pravokutnik(Tocka a, Tocka c)
+    {
+        this.a = a;
+        this.c = c;
+    }
+
+    public override double Opseg()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override double Povrsina()
+    {
+        throw new NotImplementedException();
+    }
+}
